01fb93ee1df8054978ffa827ce6efefcfaea8912
zerosnake0 <zerosnake0@gmail.com>
2020-06-01 22:04:57 +0800

    add tinyify

--- build.sh
+++ build.sh
@@ -1,8 +1,19 @@
 set -ex
 
-sh ./build_client.sh
+./node_modules/.bin/browserify \
+    -p tinyify \
+    -t browserify-css \
+    -o static/dist/client.js \
+    client.js
 
 ./node_modules/.bin/browserify \
+    -p tinyify \
+    -t browserify-css \
+    -o static/dist/create.js \
+    create.js
+
+./node_modules/.bin/browserify \
+    -p tinyify \
     -o build/server.js \
     --node \
     server.js
