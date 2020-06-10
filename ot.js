40f5a5be8b7a58803e41f3efdd9a4f8cff1ff49b
zerosnake0 <zerosnake0@gmail.com>
2020-06-10 23:29:56 +0800

    fix presence transform (from remote source)

--- ot.js
+++ ot.js
@@ -10,6 +10,10 @@ type.transformPresence = (range, ops, source) => {
     if (range === undefined || range === null) {
       return range;
     }
+    // no transform if source is remote
+    if (!source) {
+      return range;
+    }
     return type.transformPosition(range, ops);
   } finally {
     log.groupEnd();
